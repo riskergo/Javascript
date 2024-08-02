d69533f42eb320b1cfbe57b7ad1951dc08c0da06
riskergo <riski.sputra199@gmail.com>
2024-08-02 20:58:42 +0700

    number data types

---
+++ Fundamental/02-Numbers/numbers.js
@@ -0,0 +1,31 @@
+let decimal = 3.14;
+
+let integer = 10;
+
+var negative = -19;
+
+let exponen = 123e5;
+
+let octal = 0o22;
+
+let hexadecimal = 0xFF;
+
+let infinty = 2 / 0;
+
+let minusinfinity = -2 / 0;
+
+let notAnumber = 10 / "string";
+
+let anomaliNumber = 100 / "10";
+
+
+console.log(decimal);
+console.log(integer);
+console.log(negative);
+console.log(exponen);
+console.log(octal);
+console.log(hexadecimal);
+console.log(infinty);
+console.log(minusinfinity);
+console.log(notAnumber);
+console.log(anomaliNumber);

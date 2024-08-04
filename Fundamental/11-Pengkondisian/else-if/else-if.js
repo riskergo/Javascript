a313a968442815781bace4539422264736c6af4d
riskergo <riski.sputra199@gmail.com>
2024-08-04 20:41:52 +0700

    tambahan kondisi untuk pengkondisian if-(else-if)-else

---
+++ Fundamental/11-Pengkondisian/else-if/else-if.js
@@ -0,0 +1,10 @@
+let angka = prompt("Masukkan angka");
+
+if(angka % 2 === 0) {
+    alert(angka + " adalah genap");
+}else if (angka % 2 === 1) {
+    alert(angka + " adalah ganjil");
+}else {
+    alert("yang anda masukkan bukan angka");
+}
+

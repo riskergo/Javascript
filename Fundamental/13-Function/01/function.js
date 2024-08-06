457ab6999ca62052ee62c0bf3aec0a001b9117c8
riskergo <riski.sputra199@gmail.com>
2024-08-06 14:41:53 +0700

    function di javascript

---
+++ Fundamental/13-Function/01/function.js
@@ -0,0 +1,9 @@
+function menjumlahkanVolumeKubus (sisiKubus1, sisiKubus2){
+    sisiKubus1 = sisiKubus1 * sisiKubus1 * sisiKubus1;
+    sisiKubus2 = sisiKubus2 * sisiKubus2 * sisiKubus2;
+
+    return sisiKubus1 + sisiKubus2;
+}
+
+
+console.log(menjumlahkanVolumeKubus(8,3));

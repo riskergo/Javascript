96ecb2e464d4b4c2216218ec1edbcc575b938b6a
riskergo <riski.sputra199@gmail.com>
2024-08-08 17:34:30 +0700

    pengenalan array di javascript

---
+++ Fundamental/14-Array/01/array.js
@@ -0,0 +1,6 @@
+// membuat array 
+
+let hari = ['senin', 'selasa', 'rabu', 'kamis', 'jum\'at', 'sabtu', 'minggu'];
+
+// panggil salah satu element array menggunakan indexnya
+console.log(hari[4]);

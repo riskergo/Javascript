621cd30c1c07521652907f791f33540b75593673
riskergo <riski.sputra199@gmail.com>
2024-09-20 21:25:54 +0700

    menambahkan contoh kasus penggunaan arrow function pada DOM Manipulation

--- JS-Lanjutan/04-This-in-arrow-function/this.js
+++ JS-Lanjutan/04-This-in-arrow-function/this.js
@@ -57,19 +57,19 @@
 
 
 //* contoh kasus lain
-const Mahasiswa = function() {
-    this.nama = "okare";
-    this.umur = 12;
-    this.sayHello = function() {
-        console.log(`yo man ${this.nama} im ${this.umur} year old`)
-    }
+// const Mahasiswa = function() {
+//     this.nama = "okare";
+//     this.umur = 12;
+//     this.sayHello = function() {
+//         console.log(`yo man ${this.nama} im ${this.umur} year old`)
+//     }
 
     //* di sini terdapat perbedaan konsep hoisting pada pembuatan function
     //* dengan menggunakan function expression functionnya disimpan kedalam variabel dan tidak kena hoisting
     //* dengan menggunakan function declaration akan meng-hoisting dan karena hoisting, function declaration akan mengecek variabel value di scope global-nya
 
     //* contoh:
-    // setInterval(function () {       //* menggunakan function declaration
+    // setInterval(function () {       //* menggunakan function declaration yaitu setInterval tapi dengan parameter anonymous function expression biasa bukan arrow function
         // console.log(this.umur++);       //* NaN karena mencari this di scope tidak ada, maka mencari di scope global
     //     //* coba gunakan ini
         // console.log(Mahasiswa.umur) //* undefined 
@@ -82,8 +82,89 @@ const Mahasiswa = function() {
     // }, 700);
     
 
-}
+// }
+// const satu = new Mahasiswa();
 
-const satu = new Mahasiswa();
+
+
+//* kasus real untuk arrow function
+
+
+const capturedBox = document.querySelector(".box");
+
+
+//* versi errornya:
+
+// capturedBox.addEventListener("click", () => {       //* menggunakan arrow function
+//     console.log(this)       //* hasilnya object windows, karena arrow function tidak memiliki konsep this, maka akan mencari ke scope luarnya
+// })
+
+capturedBox.addEventListener("click", function(){
+    // console.log(this);
+
+
+    //* sedikit debugging, karena saat di-klik kedua kalinya, transisinya terbalik
+
+    let satu = "size";
+    let dua = "caption";
+
+    //* versi ribet
+    // if(this.classList.contains(satu)){
+    //     this.classList.toggle(dua);
+    //     setTimeout(() => {
+    //         this.classList.toggle(satu);   //* toggle lagi
+    //         //* karena ini adalah function declaration, maka gunakan arrow function agar memungkinkan terjadinya lexical scope
+            
+    //         }, 700);
+    // }else {
+    //     this.classList.toggle(satu)       //* gunakan toggle, jangan add. Karena konsep yang dibawa oleh toggle adalah: jika ada: hapus, jika tidak ada: tambahkan
+    //     setTimeout(() => {
+    //         this.classList.toggle(dua);   //* toggle lagi
+    //         //* karena ini adalah function declaration, maka gunakan arrow function agar memungkinkan terjadinya lexical scope
+            
+    //     }, 700);
+    // }
+
+    //* versi mudah :
+    if(this.classList.contains(satu)){
+        //* biasanya membutuhkan variabel temporary
+        // temp = satu;
+        // satu = dua;
+        // dua = temp;
+        //* dengan javascript terbaru, bisa mempersingkatnya dengan menggunakan ini
+        [satu, dua] = [dua, satu];
+    };
+
+    this.classList.toggle(satu);
+
+    setTimeout(()=> {
+        this.classList.toggle(dua);
+    }, 650);
+    
+
+    
+
+
+
+
+
+
+
+
+    // //* coba menggunakan anonymous function expression
+    // setTimeout(function () {
+    //     capturedBox.classList.toggle("caption");   //* tapi tidak menggunakan keyword this, tetapi nama object yang ditangkap
+    //     //* ternyata bisa, karena bukan instansiasi dari sebuah object create dari constructor function (prototype) / class
+    // }, 650);
+
+    //* cara lama
+    // let that = this;
+    // //* diakali dengan membuat variabel yang berisi this
+    // setTimeout(function () {
+    //     that.classList.toggle("caption");   //* tapi tidak menggunakan keyword this, tetapi nama object yang ditangkap
+    //     //* ternyata bisa, karena bukan instansiasi dari sebuah object create dari constructor function (prototype) / class
+    // }, 700);
+});
+
 
 

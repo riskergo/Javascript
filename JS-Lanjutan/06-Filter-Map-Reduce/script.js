4e382e96342066e646a6b79ee2954ef9a1c29014
riskergo <riski.sputra199@gmail.com>
2024-10-05 21:41:18 +0700

    array.prototype.reduce dan chaining method

--- JS-Lanjutan/06-Filter-Map-Reduce/script.js
+++ JS-Lanjutan/06-Filter-Map-Reduce/script.js
@@ -1,5 +1,6 @@
 
 //* filter, map and reduce
+//* ketiganya menghasilkan variabel baru jadi seperti membuat function expression
 //* adalah contoh dari high order functionz
 
 const numbers = [-1, 8, 9, 1, 4, -5, -4, 3, 2, 9]
@@ -35,7 +36,8 @@ for(let i = 0; i < numbers.length; i++){
 //* mengalikan semua elemen dengan angka 2
 //* dan coba urutkan 
 const angkaX2 = numbers.map(a => a * 2).sort((a,b) => a - b);
-console.log(angkaX2.join(", "));
+// console.log(numbers.join(", "));
+// console.log(angkaX2.join(", "));
 
 //* ingat pada parameter callback function map, filter ataupun forEach yang pertama merepresentasikan sebuah nilai/element dari array tsb
 //* yang kedua merepresentasikan sebuah index dari array tsb
@@ -56,13 +58,23 @@ console.log(angkaX2.join(", "));
     //* parameter dalam method reduce ada 2, 1 disebut accumulator dan 2 disebut currentValue
     //* dan sebenarnya ada nilai awalnya, yang secara default di-set 0
 //* const numbers = [-1, 8, 9, 1, 4, -5, -4, 3, 2, 9] 
-const totalInArrayNumbers = numbers.reduce( function(accumulator, currentValue){
+const totalInArrayNumbers = numbers.reduce( function(accumulator, currentValue){return accumulator + currentValue}, 5); //* tambahkan ,5 setelah block kurung kurawal
     // console.log(accumulator)
     // console.log(currentValue)
-    // return accumulator + currentValue, 5}
-}); //* salah, harusnya letak ,5 setelah block kurung kurawal
+const totalInArrayNumbers2 = numbers.reduce( (accumulator, currentValue) => accumulator + currentValue,5 ) //* karena ingin menjumlahkan, maka gunakan operasi tambah // *31 juga
+//* yang terjadi adalah : -1 + 8 + 9 + 1 + 4 + -5 + -4 + 3 + 2 + 9 = 31;
+//* maka hasilnya sama dengan yang di atas ini
+//* untuk reduce sebenarnya ada 3 parameter yaitu, accumulator, currentValue, dan initialValue
+//* initialValue di-set secara default adalah 0 jika tidak dituliskan seperti ini 0 + -1 + 8 + 9 + 1 + 4 + -5 + -4 + 3 + 2 + 9 = 26
+// console.log(totalInArrayNumbers);
+// console.log(totalInArrayNumbers2);
 
-const totalInArrayNumbers2 = numbers.reduce( (accumulator, currentValue) => accumulator + currentValue, 5 )
-console.log(totalInArrayNumbers);
-console.log(totalInArrayNumbers2);
+//* method chaining
+//* contoh kasus: cari element array yang > 5
+//* lalu kalikan dengan 2 ( * 2);
+//* dan terakhir jumlahkan semuanya
 
+const hasil = numbers.filter( (elementArray) => elementArray > 5  )     //* 8, 9, 9
+    .map( (elementArray) => elementArray * 3 )  //* 24, 27, 27
+    .reduce( (accumulator, currentValue) => accumulator + currentValue, 0 );    //* 0 + 24 + 27 + 27
+console.log(hasil); //* 78
